9300a4892db7a6965f220d6209be3c9e28612a02
JenniferNA <jennifer.n.avila@gmail.com>
2022-03-08 02:16:06 -0500

    changed ui toolkit to MUI

--- SourceCode/hello-world-example/src/components/Navigation.js
+++ SourceCode/hello-world-example/src/components/Navigation.js
@@ -1,41 +1,40 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import logo from '../graphics/DegreEZLogo.png';
+
+
+
 
 function Navigation() {
   return (
     <div className="navigation">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            React Multi-Page Website
-          </NavLink>
-          <div>
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/">
-                  Home
-                  <span className="sr-only">(current)</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
-                  Contact
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/blog">
-                  Blog
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky"color="transparent">
+        <Toolbar>
+          <Box component="div" sx={{ flexGrow: 1 }}>
+            <img src={logo}  alt="Logo" height="50" />
+          </Box>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 0 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+
+
     </div>
   );
 }
