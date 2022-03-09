ba45baad61333a41001e65d2c1580ac695032d16
DylanJVA <dylanvanallen@gmail.com>
2022-03-09 00:16:45 -0500

    Updates to admin portal and home upload button

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -1,9 +1,34 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
 
 function AdminPortal() {
   return (
     <div className="adminportal">
-        <p>this is the admin portal</p>
+        <header>
+          <h1>Admin Portal</h1>
+          <p>Primary Administrator</p>
+          <p>University: State University of New York at Albany</p>
+        </header>
+        <h2>Master Class Link</h2>
+        <div>
+          <TextField
+            disabled
+            id="outlined-disabled"
+            label="Should retrieve link from backend"
+          />
+          <Fab color="secondary" aria-label="edit"><EditIcon /></Fab>
+      </div>
+      <h2>Semester Schedule Link</h2>
+        <div>
+        <TextField
+            disabled
+            id="outlined-disabled"
+            label="Should retrieve link from backend"
+          />
+          <Fab color="secondary" aria-label="edit"><EditIcon /></Fab>
+      </div>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,15 @@ import React,{Component} from 'react';
 import Grid from '@mui/material/Grid';
 import homebg from '../graphics/HomeBG.png';
 import axios from 'axios';
+import { styled } from '@mui/material/styles';
 
+import Button from '@mui/material/Button';
+
+
+
+const Input = styled('input')({
+  display: 'none',
+});
 
 function Home() {
 
@@ -12,6 +20,12 @@ function Home() {
       <Grid component="div" backgroundImage={homebg} backgroundSize="cover" sx={{ bgcolor: '#cfe8fc', height: '50vh', width:'100vw', flexGrow: 1 }}>
       </Grid>
       <Grid component="div" backgroundImage={homebg} backgroundSize="cover" sx={{ bgcolor: '#ffffff', height: '30vh', width:'100vw', flexGrow: 1 }}>
+      <label htmlFor="contained-button-file">
+      <Input accept="image/*" id="contained-button-file" multiple type="file" />
+        <Button variant="contained" component="span">
+          Upload Degree Audit
+        </Button>
+      </label>
       </Grid>
       <Grid component="div" backgroundImage={homebg} backgroundSize="cover" sx={{ bgcolor: '#cfe8fc', height: '100vh', width:'100vw', flexGrow: 1 }}>
       </Grid>
