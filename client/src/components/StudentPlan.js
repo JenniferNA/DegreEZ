59266ecd18375b52c5a231de4a3e1d4d35e647b7
emonzon42 <elijah.monzon42@gmail.com>
2022-05-07 18:15:27 -0400

    student planning page now looks like the sketch

--- client/src/components/StudentPlan.js
+++ client/src/components/StudentPlan.js
@@ -1,16 +1,6 @@
 
 import React, {Component} from 'react';
-import homebg from '../graphics/HomeBG.png';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
-import { Typography, Button, CssBaseline, Box, Grid, Paper, Avatar } from '@mui/material';
-import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
-import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
-
-const Input = styled('input')({
-  display: 'none',
-});
+import { FaChevronRight } from 'react-icons/fa';
 
 function StudentPlan() {
   return (
@@ -27,34 +17,53 @@ function StudentPlan() {
             </div>
 
             <div class="row">
-                <h3>Multi Semester Plan</h3><a href="#">Edit</a>
+                <h3>Multi Semester Plan</h3><a id="edit" href="#">Edit </a>
             </div>
         </div>
 
-        <div class="multisemplan">
-            <h2>Semester 20XX</h2>
-            <hr></hr>
-            <ul>
-                <li>Course 1 (X credits)</li>
-                <li>Course 2 (X credits)</li>
-                <li>Course 3 (X credits)</li>
-                <li>Course 4 (X credits)</li>
-                <li>Course 5 (X credits)</li>
-            </ul>
-            <a>Detailed View </a>
-        </div>
-
-        <div class="multisemplan">
-            <h2>Semester 20XX</h2>
-            <hr></hr>
-            <ul>
-                <li>Course 1 (X credits)</li>
-                <li>Course 2 (X credits)</li>
-                <li>Course 3 (X credits)</li>
-                <li>Course 4 (X credits)</li>
-                <li>Course 5 (X credits)</li>
-            </ul>
-            <a>Detailed View </a>
+        <div class="planlist">
+            <div class= "schedule">
+                <h2>Semester 20XX</h2>
+                <hr></hr>
+                <ul>
+                    <li>Course 1 (X credits)</li>
+                    <li>Course 2 (X credits)</li>
+                    <li>Course 3 (X credits)</li>
+                    <li>Course 4 (X credits)</li>
+                    <li>Course 5 (X credits)</li>
+                </ul>
+                <div class="row">
+                    <a href="#">Detailed View <FaChevronRight /></a>
+                </div>
+            </div>
+            <div class= "schedule">
+                <h2>Semester 20XX</h2>
+                <hr></hr>
+                <ul>
+                    <li>Course 1 (X credits)</li>
+                    <li>Course 2 (X credits)</li>
+                    <li>Course 3 (X credits)</li>
+                    <li>Course 4 (X credits)</li>
+                    <li>Course 5 (X credits)</li>
+                </ul>
+                <div class="row">
+                    <a href="#">Detailed View <FaChevronRight /></a>
+                </div>
+            </div>
+            <div class= "schedule">
+                <h2>Semester 20XX</h2>
+                <hr></hr>
+                <ul>
+                    <li>Course 1 (X credits)</li>
+                    <li>Course 2 (X credits)</li>
+                    <li>Course 3 (X credits)</li>
+                    <li>Course 4 (X credits)</li>
+                    <li>Course 5 (X credits)</li>
+                </ul>
+                <div class="row">
+                    <a href="#">Detailed View <FaChevronRight /></a>
+                </div>
+            </div>
         </div>
       
     </div>
