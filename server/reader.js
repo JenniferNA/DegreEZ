1f9bb997dac5fa8dfaf91e09b0e5afeecddbe801
DylanJVA <dylanvanallen@gmail.com>
2022-05-09 14:42:59 -0400

    added suggestive capabilities

--- server/reader.js
+++ server/reader.js
@@ -8,8 +8,8 @@ minor = "";
 
 
 const required = [];
-const taken = []
-
+const priority = [];
+const suggested = [];
 const minor_reqs = [];//
 const math_min_categories = []
 
@@ -22,6 +22,7 @@ const gen_ed_strings = ["MATHEMATICS AND STATISTICS","WRITING AND CRITICAL INQUI
 
 const major_map = new Map();
 cat_map = new Map();
+const classes_map = new Map();
 //Each major must be added to the map
 var fs = require('fs'); 
 const { parse } = require('csv-parse');
@@ -30,6 +31,12 @@ var parser = parse({relax_column_count: true, quote: '', ltrim: true, rtrim: tru
   for (i = 0; i < records.length; i++) major_map.set(records[i][0],records[i].slice(1,records[i].length-1))
 });
 fs.createReadStream('major_info.csv').pipe(parser);
+
+var parser = parse({relax_column_count: true, quote: '', ltrim: true, rtrim: true, delimiter: ',' },function (err, records) {
+  if (err) console.error("error:", err);
+  for (i = 0; i < records.length; i++) classes_map.set(records[i][0],records[i][1])
+});
+fs.createReadStream('classes.csv').pipe(parser);
 
 
 
@@ -66,14 +73,46 @@ new PdfReader().parseFileItems("quinten_audit.pdf", (err, item) => {
                 for (let k = 0; k < a.length; k++) {
                   if(!(a[k] === '') && required.includes(a[k]) == false && all.slice(all.search("====  UALBANY ACADEMIC SUMMARY  ====")+80,all.indexOf("****  LEGEND  ****")).indexOf(a[k]) == -1) {
                     required.push(a[k]);
-                    console.log(a[k]);
                   }
                 }
               }
               if (i=== (major_map.get(major)).length-1) {
-                
+                //Here get suggested classes
                 console.log("remaining classes:");
                 console.log(required)
+                for (let classindex = 0; classindex < required.length; classindex++) {
+                  //check if class has prereq
+                  if (classes_map.has(required[classindex])) {
+                    var prereqs = classes_map.get(required[classindex]).split('+');
+                      for (let reqi = 0; reqi < prereqs.length; reqi++) {
+                        if (required.includes(prereqs[reqi])) {
+                          priority.push(prereqs[reqi]);
+                        }
+                      }
+                  }
+                }
+                console.log("Priority classes:");
+                console.log(priority);
+
+                for (let classindex = 0; classindex < required.length; classindex++) {
+                  if (priority.includes(required[classindex]) && classes_map.has(required[classindex]) == false) {
+                    suggested.push(required[classindex]);
+                  }
+                  else if (priority.includes(required[classindex]) && classes_map.has(required[classindex]) == true) {
+                    var prereqs = classes_map.get(required[classindex]).split('+');
+                    var a = true;
+                    for (let reqi = 0; reqi < prereqs.length; reqi++) {
+                      if (required.includes(prereqs[reqi])) {
+                        a = false;
+                      }
+                    }
+                    if (a == true) {
+                      suggested.push(required[classindex]);
+                    }
+                  }
+                }
+                console.log("Suggested classes:");
+                console.log(suggested);
               }
               else catrecords.push(records);
             });
@@ -84,7 +123,6 @@ new PdfReader().parseFileItems("quinten_audit.pdf", (err, item) => {
 
         math_minor = all.slice(all.search("Mathematics Minor"))
         
-        console.log(required);
         console.log(major_reqs);
         console.log(gen_ed_reqs);
         console.log("Major: ",major);
