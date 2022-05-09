8b33d5d01c07b9ad14fbf77fd649919cb3f8dbae
Patrizzia Espina <pespina@albany.edu>
2022-05-09 15:24:18 -0400

    Added menu function to navigate to different pages

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,41 +1,71 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import logo from '../graphics/DegreEZLogo.png';
-
-
-
+import { MenuList, Menu, MenuItem, Button, Box, Typography, AppBar, Toolbar } from "@mui/material";
+import { makeStyles } from "@material-ui/core/styles";
+import { useState } from "react";
 
 function Navigation() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className="navigation">
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky"color="transparent">
-        <Toolbar>
-          <Box component="div" sx={{ flexGrow: 1 }}>
-            <a href="/">
-              <img src={logo} alt="Logo" height="50" />
-            </a>
-          </Box>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 0 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
-
+        <AppBar position="sticky" color="transparent">
+          <Toolbar>
+            <Box component="div" sx={{ flexGrow: 1 }}>
+              <a href="/">
+                <img src={logo} alt="Logo" height="50" />
+              </a>
+            </Box>
+            <IconButton
+              onClick={handleClick}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 0 }}
+              >              
+              <MenuIcon />              
+            </IconButton>
+          </Toolbar>
+        </AppBar>     
+      </Box>
+      <Menu  
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+        > 
+        
+          <MenuItem 
+            href="/StudentPlan"
+            target="_blank"
+            component="a" 
+            onClick={handleClose}>
+            Student Plan
+          </MenuItem>
+        
+          <MenuItem 
+            href="/About"          
+            target="_blank"
+            component="a" 
+            onClick={handleClose} 
+            >
+            About
+          </MenuItem>      
+      </Menu>
+              
 
     </div>
   );
