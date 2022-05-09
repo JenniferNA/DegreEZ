138d3c3a3643844071dc0566370130aa3252280c
Patrizzia Espina <pespina@albany.edu>
2022-05-09 15:35:37 -0400

    Added menu function to navigate to different pages and changed About page text

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -15,10 +15,10 @@ function About() {
           <div class="col-lg-5">
             <h1 class="font-weight-light">About</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              DegreEZ is a tool designed to help students with planning their schedules. It can take into account your degree audit and suggest the optimal schedule based on
+              the remaining classes needed in your major and classes you might specifically want to take. It will always be updated with the latest class schedule and degree 
+              requirements and any issues can be resolved manually by an admin. This project was developed by 5 UAlbany students: Dylan VanAllen, Jennifer Avila, Patrizzia Espina, 
+              Duckhawn Baek, and Elijah Monzon.
             </p>
           </div>
         </div>
