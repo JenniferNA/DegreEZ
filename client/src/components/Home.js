5abacc955ef23a0eb0a8c49781e826adbf8c54b9
JenniferNA <jennifer.n.avila@gmail.com>
2022-05-08 23:51:49 -0400

    File Upload Added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,36 @@
 import React, {Component} from 'react';
 import homebg from '../graphics/HomeBG.png';
-import axios from 'axios';
-import PropTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
-import { Typography, Button, CssBaseline, Box, Grid, Paper, Avatar } from '@mui/material';
+import { Typography, Button, CssBaseline, Box, Grid, Paper, Avatar, Stack } from '@mui/material';
 import SchoolOutlinedIcon from '@mui/icons-material/SchoolOutlined';
 import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import axios from 'axios';
 
 const Input = styled('input')({
   display: 'none',
 });
 
 function Home() {
+
+  //pdf upload
+  const uploadHandler = (e) => {
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+
+    const formData = new FormData();
+    formData.append('file', file);
+    
+    axios.post('/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  }
+  
   return (
     <div className="home">
-      <Grid container component="top">
-        <Grid>
+      <Grid container>
+        <Grid component="top">
           <Box
             class="homebg"
             style={{
@@ -24,30 +39,24 @@ function Home() {
             height: '50vh',
             width:'100vw'
             }}>
-              <Typography variant="h5" align="center" color="white">Understanding your Degree</Typography>
-              <Typography variant="h2" align="center" color="white">Made easier</Typography>
-              <Typography variant="h5" align="center" color="white">
+              <Typography variant="h5" textAlign="center" color="white">Understanding your Degree</Typography>
+              <Typography variant="h2" textAlign="center" color="white">Made easier</Typography>
+              <Typography variant="h5" textAlign="center" color="white">
                 DegreEZ is an online tool designed to empower students to take control of their academic career
               </Typography>
           </Box>
         </Grid>          
-        <Grid container component="middle" justify="center" alignItems="center" height='25vh'>
-          <Grid item>
-            <Typography variant="h5" align='center'> 
+        <Stack component="middle" justifyContent="center" alignItems="center" height='25vh' width='100vw'>
+            <Typography variant="h5" height='10vh'> 
               To get started, just upload a PDF of your degree audit
             </Typography>
-          </Grid>           
-                   
-          <Grid item component="upload" justify="center"  alignItems="center">           
             <label htmlFor="contained-button-file">
-            <Input accept=".pdf/*" id="contained-button-file" multiple type="file" />
+            <Input accept="application/pdf" id="contained-button-file" type="file" height='10vh' onChange={uploadHandler}/>
               <Button push variant="contained" component="span">
                 Upload Degree Audit
               </Button>
-            </label>            
-          </Grid>
-                   
-        </Grid>
+            </label>
+        </Stack>
         <Grid container component="bottom" backgroundSize="cover" sx={{ bgcolor: '#8db4ce', height: '25vh', width:'100vw'}}>
           <Typography variant="h5" align="center" color="white">Why use DegreEZ</Typography>
           <Avatar sx={{ m: 1, color: '#8db4ce', bgcolor: 'white' }}>
---
+++ server.js
@@ -0,0 +1,45 @@
+/*
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/url"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
+*/
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+  file.mv(`${__dirname}/server/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: file.name, filePath: `/server/uploads/${file.name}` });
+  });
+});
+
+app.listen(5000, () => console.log('Server Started...'));
